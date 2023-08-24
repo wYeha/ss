62ea898bdaa232142473dd2afa12018c84940c72
wyeha <snovavzloman@mail.ru>
2023-08-24 15:01:25 +0300

    2nd done

---
+++ Example2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = new Random().Next(1, 10), b = new Random().Next(1, 10), c = new Random().Next(1, 10), max = a;
+string s = $"a, b, c = {a}, {b}, {c}";
+Console.WriteLine(s);
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = " + max);
