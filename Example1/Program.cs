8abc424173b43ffe452a60bc0d010e99b69ead06
wyeha <snovavzloman@mail.ru>
2023-08-24 14:38:36 +0300

    1st done

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+
+int max;
+int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+
+Console.Write("a=");
+Console.WriteLine(a);
+Console.Write("b=");
+Console.WriteLine(b);
+
+if (a > b) max = a;
+else max = b;
+
+Console.Write("max=");
+Console.WriteLine(max);
